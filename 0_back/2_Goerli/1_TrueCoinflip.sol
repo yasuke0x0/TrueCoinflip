b58b0bbdec1ed6de7df4df51463ef83f54a2f7d0
0xheartcode <0xheartcode@gmail.com>
2023-01-24 00:53:13 +0100

    `placeBet`ETH

--- 0_back/2_Goerli/1_TrueCoinflip.sol
+++ 0_back/2_Goerli/1_TrueCoinflip.sol
@@ -76,10 +76,15 @@ contract TrueCoinflip is VRFConsumerBaseV2, ConfirmedOwner, ReentrancyGuard {
 
     // Polyroll START (´・ω・｀)
     // Each bet is deducted 100 basis points (1%) in favor of the house
-    uint public houseEdgeBP = 100;
+    // uint public houseEdgeBP = 100;
+
+    //hijacked this variable to turn it into 190% profit if win. This variable has been MODIFIED from source.
+    uint public houseEdgeBP = 190;
 
     uint public minBetAmount = 1 ether;
     uint public maxBetAmount = 100 ether;
+
+    uint public balanceMaxProfitRatio = 24; // might remove, not needed with hardcoded Profit Ratio.
     
         // Funds that are locked in potentially winning bets. Prevents contract from committing to new bets that it cannot pay out.
     uint public lockedInBets;
@@ -113,8 +118,8 @@ contract TrueCoinflip is VRFConsumerBaseV2, ConfirmedOwner, ReentrancyGuard {
     int public houseProfit;
 
     // Events
-    event BetPlaced(uint indexed betId, address indexed gambler, uint amount, uint8 indexed modulo, uint8 rollUnder, uint40 mask);
-    event BetSettled(uint indexed betId, address indexed gambler, uint amount, uint8 indexed modulo, uint8 rollUnder, uint40 mask, uint outcome, uint winAmount, uint rollReward);
+    event BetPlaced(uint indexed betId, address indexed gambler, uint amount);
+    event BetSettled(uint indexed betId, address indexed gambler, uint amount, uint outcome, uint winAmount);
     event BetRefunded(uint indexed betId, address indexed gambler, uint amount);
 
     // used to top up the contract.
@@ -133,9 +138,169 @@ contract TrueCoinflip is VRFConsumerBaseV2, ConfirmedOwner, ReentrancyGuard {
         return bets.length;
     }
 
+    // Returns maximum profit allowed per bet. Prevents contract from accepting any bets with potential profit exceeding maxProfit.
+    function maxProfit() public view returns (uint) {
+        return address(this).balance / balanceMaxProfitRatio;
+    }
+
+    // Set balance-to-maxProfit ratio. 
+    function setBalanceMaxProfitRatio(uint _balanceMaxProfitRatio) external onlyOwner {
+        balanceMaxProfitRatio = _balanceMaxProfitRatio;
+    }
+
+    // Set minimum bet amount. minBetAmount should be large enough such that its house edge fee can cover the Chainlink oracle fee.
+    function setMinBetAmount(uint _minBetAmount) external onlyOwner {
+        minBetAmount = _minBetAmount;
+    }
+
+    // Set maximum bet amount. Setting this to zero effectively disables betting.
+    function setMaxBetAmount(uint _maxBetAmount) external onlyOwner {
+        maxBetAmount = _maxBetAmount;
+    }
+
+    // Set house edge.
+    function setHouseEdgeBP(uint _houseEdgeBP) external onlyOwner {
+        houseEdgeBP = _houseEdgeBP;
+    }
+
+    // Owner can withdraw funds not exceeding balance minus potential win amounts by open bets.
+    function withdrawFunds(address payable beneficiary, uint withdrawAmount) external onlyOwner {
+        require(withdrawAmount <= address(this).balance - lockedInBets, "Withdrawal exceeds limit");
+        beneficiary.transfer(withdrawAmount);
+    }
+
+    // Owner can withdraw non-MATIC tokens.
+    function withdrawToken(address _token) external onlyOwner {
+        IERC20(_token).safeTransfer(owner(), IERC20(_token).balanceOf(address(this)));
+    }
+
+    // Returns the expected win amount. This function has been MODIFIED from source.
+    function getWinAmount(uint _amount) private view returns (uint winAmount) {
+        uint houseEdgeFee = _amount * (houseEdgeBP) / 100;
+        winAmount = (houseEdgeFee);
+    }
+
+    //working with `placeBet`, `settleBet` and `refundBet` on ETH and not IERC token.
+    // That will be step 2.
+
+    // Place bet
+    function placeBet() external payable nonReentrant {
+
+        // Validate input data.
+        uint amount = msg.value;
+
+        // Winning amount.
+        uint possibleWinAmount = getWinAmount(amount);
+
+        // Enforce max profit limit. Bet will not be placed if condition is not met.
+        require(possibleWinAmount <= amount + maxProfit(), "maxProfit violation");
+
+        // Check whether contract has enough funds to accept this bet.
+        require(lockedInBets + possibleWinAmount <= address(this).balance, "Insufficient funds");
+
+        // Update lock funds.
+        lockedInBets += possibleWinAmount;
+
+        // Request random number from Chainlink VRF. Store requestId for validation checks later.
+        // Commenting the following line out, not sure how to resolve this conflict.
+        uint256 requestIdMod = requestRandomWords();
+
+        // Map requestId to bet ID.
+        betMap[requestIdMod] = bets.length;
+
+        // Record bet in event logs. Placed before pushing bet to array in order to get the correct bets.length.
+        emit BetPlaced(bets.length, msg.sender, amount);
+
+        // Store bet in bet list.
+        bets.push(Bet(
+            {
+                amount: amount,
+                placeBlockNumber: block.number,
+                gambler: payable(msg.sender),
+                isSettled: false,
+                outcome: 0,
+                winAmount: 0
+            }
+        ));
+    }
+
+    // Settle bet. Function can only be called by fulfillRandomness function, which in turn can only be called by Chainlink VRF.
+    function settleBet(uint _requestIdMod, uint _randomNumber) internal nonReentrant {
+        
+        uint betId = betMap[_requestIdMod];
+        Bet storage bet = bets[betId];
+        uint amount = bet.amount;
+        
+        // Validation checks.
+        require(amount > 0, "Bet does not exist");
+        require(bet.isSettled == false, "Bet is settled already");
+
+        // Fetch bet parameters into local variables (to save gas).
+        address payable gambler = bet.gambler;
+
+        // Do a roll by taking a modulo of random number.
+        uint outcome = _randomNumber % 2 + 1;
+
+        // Win amount if gambler wins this bet
+        uint possibleWinAmount = getWinAmount(amount);
+
+        // Actual win amount by gambler.
+        uint winAmount = 0;
+
+        if (outcome == 1 ) {
+                winAmount = possibleWinAmount;
+            } else { // do nothing 
+        }
+
+        winAmount = possibleWinAmount;
+
+        // Unlock possibleWinAmount from lockedInBets, regardless of the outcome.
+        lockedInBets -= possibleWinAmount;
+
+        // Update bet records
+        bet.isSettled = true;
+        bet.winAmount = winAmount;
+        bet.outcome = outcome; // 0, not set, 1 =
+
+        // Send prize to winner, add ROLL reward to loser, and update house profit.
+        if (winAmount > 0) {
+            houseProfit -= int(winAmount - amount);
+            gambler.transfer(winAmount);
+        } else {
+            houseProfit += int(amount);
+        }
+        
+        // Record bet settlement in event log.
+        emit BetSettled(betId, gambler, amount, outcome, winAmount);
+    }
+
+    function refundBet(uint betId) external nonReentrant {
+        
+        Bet storage bet = bets[betId];
+        uint amount = bet.amount;
+
+        // Validation checks
+        require(amount > 0, "Bet does not exist");
+        require(bet.isSettled == false, "Bet is settled already");
+        require(block.number > bet.placeBlockNumber + 21600, "Wait before requesting refund");
+
+        uint possibleWinAmount = getWinAmount(amount);
+
+        // Unlock possibleWinAmount from lockedInBets, regardless of the outcome.
+        lockedInBets -= possibleWinAmount;
+
+        // Update bet records
+        bet.isSettled = true;
+        bet.winAmount = amount;
+
+        // Send the refund.
+        bet.gambler.transfer(amount);
+
+        // Record refund in event logs
+        emit BetRefunded(betId, bet.gambler, amount);
+    }
 
     // Polyroll END (´・ω・｀)
-
 
 
     constructor( ) VRFConsumerBaseV2(0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D) ConfirmedOwner(msg.sender) {
@@ -155,9 +320,17 @@ contract TrueCoinflip is VRFConsumerBaseV2, ConfirmedOwner, ReentrancyGuard {
     
     function fulfillRandomWords( uint256 _requestId, uint256[] memory _randomWords) internal override {
     require(s_requests[_requestId].exists, "request not found");
-    s_requests[_requestId].fulfilled = true;
-    s_requests[_requestId].randomness = (_randomWords[0] % 2) + 1; // return 0 on not set, 1 on even and 2 uneven.
+    settleBet(_requestId, _randomWords[0]);
+    s_requests[_requestId].randomWords = _randomWords;
+    s_requests[_requestId].fulfilled = true; // delete this if not necessary.
+    s_requests[_requestId].randomness = (_randomWords[0] % 2) + 1; // return 0 on not set, 1 win, 2 lose. delete this if not necessary.
     }
+
+    function getRequestStatus(uint256 _requestId) external view returns (bool fulfilled, uint randomness, uint256[] memory randomWords) {
+    require(s_requests[_requestId].exists, "request not found");
+    RequestStatus memory request = s_requests[_requestId];
+    return (request.fulfilled, request.randomness, request.randomWords);
+    }   
     // Chainlink function END
 
 
